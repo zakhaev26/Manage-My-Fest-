d27bc5f464722a104688d934baef93abf35f516a
Soubhik <b422056@iiit-bh.ac.in>
2023-03-01 16:38:38 +0530

    basic setup

---
+++ Transport.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef struct geographical_DISTANCE{
+
+	char state[20];
+	float distanceByTrain;
+	float distanceByFlight;
+	float distanceByBus;
+
+}gd;
+
+
+
+void transport(){
+
+	//welcome message to greet user.
+	printf("Welcome to transport facility!");
+	printf("\n");
+
+	//user's state extraction
+	printf("In which state do you live?:");
+	char locationOfUser[20];
+	scanf("%s",&locationOfUser);
+	// if(strcmp("punjab",locationOfUser)==0){
+	// 	printf("same");
+	// }
+	// else{
+	// 	printf("none");
+	// }
+	//database:
+
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+	gd punjab={};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// if(strcmp(locationOfUser,"punjab")==0){
+	// 	//distance of punjab from bbs:
+	// 	distance=2099 ;
+	// 	printf("distance is %f",distance);
+	// }
+
+}
+
+
+
+
+int main(){
+
+// printf("%d",strcmp("dog","dog"));
+
+transport();
+
+
+
+
+return 0;
+}
